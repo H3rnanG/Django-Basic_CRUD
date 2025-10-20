7a3ee539f221c55f9bdb7c53855f041ebd3d2533
h3rnan <h3rnangamarr@gmail.com>
2025-10-20 18:24:37 -0500

    feat: enhance permissions and views for note management; add my_notes action and improve queryset filtering

--- notes/permissions.py
+++ notes/permissions.py
@@ -2,5 +2,14 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwnerOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return True
+    
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or request.user.is_staff
+        if request.method in SAFE_METHODS and obj.is_published:
+            return True
+        
+        if request.user and request.user.is_staff:
+            return True
+
+        return obj.user == request.user
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated, DjangoModelPermissions, SAFE_METHODS
+from django.db.models import Q
 from .serializers import NoteSerializer, CategorySerializer
 from .permissions import IsOwnerOrAdmin
 from .models import Note, Category
@@ -8,16 +11,38 @@ from .models import Note, Category
 class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = [IsOwnerOrAdmin]
 
-    def get_permissions(self):
-        if self.request.method in SAFE_METHODS:
-            return [AllowAny()]
-        elif self.request.method == 'POST':
-            return [IsAuthenticated()]
-        return [IsOwnerOrAdmin()]
+    def get_queryset(self):
+        user = self.request.user
+        
+        if self.action == 'list':
+            if user.is_authenticated:
+                return Note.objects.filter(Q(user=user) | Q(is_published=True))
+            return Note.objects.filter(is_published=True)
+        
+        return Note.objects.all()
+
+    # def get_permissions(self):
+    #     if self.request.method in SAFE_METHODS:
+    #         return [AllowAny()]
+    #     elif self.request.method == 'POST':
+    #         return [IsAuthenticated()]
+    #     return [IsOwnerOrAdmin()]
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+    @action(detail=False, methods=['get'], url_name='my-notes', url_path='my-notes')
+    def my_notes(self, request):
+        user = request.user
+        
+        if not user.is_authenticated:
+            return Response({'detail': 'You do not have permission to perform this action.'}, status=401)
+        
+        notes = Note.objects.filter(user=user)
+        serializer = self.get_serializer(notes, many=True)
+        return Response(serializer.data)
 
 
 class CategoryViewSet(ModelViewSet):
