9106e96d584b2f036f85df2100c6a0a9f9f48454
h3rnan <h3rnangamarr@gmail.com>
2025-10-29 17:28:05 +0000

    feat: enhance NoteFilter with additional filters for user and date range; update fields in Meta class

--- notes/filters.py
+++ notes/filters.py
@@ -2,7 +2,18 @@ from django_filters import rest_framework as filters
 from .models import Note
 
 class NoteFilter(filters.FilterSet):
+    user = filters.NumberFilter()
+    category = filters.CharFilter(field_name='category__title', lookup_expr='iexact')
+    created_after = filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
+    created_before = filters.DateTimeFilter(field_name="created_at", lookup_expr="lte")
+    
+    ordering = filters.OrderingFilter(
+        fields={
+            ('category', 'category'),
+            ('created_at', 'created_at')
+        }
+    )
     
     class Meta:
         model = Note
-        fields = ['category', 'is_published', 'created_at']
+        fields = ['is_published', 'user', 'category']
--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=50)
@@ -10,6 +11,7 @@ class Category(models.Model):
     
     def __str__(self):
         return self.title
+    
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
