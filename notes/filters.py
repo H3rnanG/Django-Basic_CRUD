30c750ab6701d7a7716d6b7c1bbc629d5c7a450f
h3rnan <h3rnangamarr@gmail.com>
2025-10-29 22:47:33 +0000

    feat: add full-text search filter to NoteFilter and update fields in Meta class

--- notes/filters.py
+++ notes/filters.py
@@ -1,11 +1,13 @@
 from django_filters import rest_framework as filters
+from django.contrib.postgres.search import SearchVector, SearchQuery
 from .models import Note
 
 class NoteFilter(filters.FilterSet):
     user = filters.NumberFilter()
     category = filters.CharFilter(field_name='category__title', lookup_expr='iexact')
-    created_after = filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
-    created_before = filters.DateTimeFilter(field_name="created_at", lookup_expr="lte")
+    created_after = filters.DateTimeFilter(field_name='created_at', lookup_expr='gte')
+    created_before = filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
+    search = filters.CharFilter(field_name='description', method='search_fulltext')
     
     ordering = filters.OrderingFilter(
         fields={
@@ -13,6 +15,13 @@ class NoteFilter(filters.FilterSet):
             ('created_at', 'created_at')
         }
     )
+
+    def search_fulltext(self, queryset, field_name, value):
+        if not value:
+            return queryset
+        return queryset.annotate(
+            search=SearchVector('title', 'description')
+        ).filter(search=SearchQuery(value))
     
     class Meta:
         model = Note
